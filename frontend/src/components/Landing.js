f031534891b48f0efe4cee2a6e3a3aa13691646a
athensbird <markwang@utexas.edu>
2018-10-20 18:13:52 -0500

    setup sidebar

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,15 +1,51 @@
 import React from 'react';
+
+import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+
+import SideBar from './SideBar';
+
+const styles = {
+  card: {
+    width: '30%',
+    margin: '2rem auto',
+  },
+  grid: {
+    'margin-top': '5rem',
+  },
+};
 
 class Landing extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      sideBarOpen: true,
+    };
+    this.toggleSideBar = this.toggleSideBar.bind(this);
   }
-  render(){
+  toggleSideBar() {
+    this.setState((prevState) => ({
+      sideBarOpen: !prevState.sideBarOpen,
+    }));
+  }
+  render() {
+    const { classes } = this.props;
     return (
-      <p>testing</p>
+      <Grid className={classes.grid}>
+        <SideBar
+          open={this.state.sideBarOpen}
+          toggleSideBar={this.toggleSideBar}
+        />
+        <Card className={classes.card}>
+          <p>Get Help</p>
+        </Card>
+        <Card className={classes.card}>
+          <p>Offer Help</p>
+        </Card>
+      </Grid>
     );
   }
 }
 
-export default Landing;
+export default withStyles(styles)(Landing);
---
+++ frontend/src/components/SideBar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  list: {
+    width: '15rem',
+  },
+  listItem: {
+    'text-align': 'left',
+    'font-family': 'system-ui',
+  },
+};
+
+const SideBar = (props) => {
+  const { classes, open, toggleSideBar } = props;
+  const sideList = (
+    <div className={classes.list}>
+      <List component="nav">
+        <ListItem
+          button
+          className={classes.listItem}
+        >
+          <ListItemText primary="Profile" />
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+          className={classes.listItem}
+        >
+          <ListItemText primary="Bookmarked Projects" />
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+          className={classes.listItem}
+        >
+          <ListItemText primary="Joined Projects" />
+        </ListItem>
+        <Divider/>
+        <ListItem
+          button
+          className={classes.listItem}
+        >
+          <ListItemText primary="Community Calendar" />
+        </ListItem>
+        <Divider/>
+      </List>
+    </div>
+  );
+
+  return (
+    <Drawer open={open} onClose={toggleSideBar} anchor="left">
+      <div
+       tabIndex={0}
+       role="button"
+       onClick={toggleSideBar}
+       onKeyDown={toggleSideBar}
+     >
+       {sideList}
+     </div>
+    </Drawer>
+  )
+}
+
+export default withStyles(styles)(SideBar);
