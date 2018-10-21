2ac794fe1f9e672e5f13faf6ff558a03ac125399
athensbird <markwang@utexas.edu>
2018-10-21 01:21:56 -0500

    setup createBeacon container

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import { Route } from "react-router-dom";
+import CreateTask from '../components/CreateTask';
 import Landing from '../components/Landing';
 
 const Routes = () => (
   <div>
     <Route exact path="/" component={Landing} />
+    <Route exact path="/create" component={CreateTask} />
   </div>
 );
 
