3f2b3f9bb38d6b85fde6afe65f1305ecde685d3e
黃秋瑾 <chioujiin@chioujiin.local>
2018-10-21 10:52:13 -0500

    waiting page done

--- frontend/src/components/waiting.js
+++ frontend/src/components/waiting.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Typography from '@material-ui/core/Typography';
 
 const styles = theme => ({
   progress: {
@@ -31,6 +32,10 @@ class CircularDeterminate extends React.Component {
     const { classes } = this.props;
     return (
       <div>
+        <Typography component="h5" variant="h5">
+          Waiting for Helper
+        </Typography>
+
         <CircularProgress
           className={classes.progress}
           variant="determinate"
