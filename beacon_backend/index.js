ac1a9182c0abe8c1fd41cf4595081405c195b064
devamitr <dev.amitr@gmail.com>
2018-10-21 04:22:43 -0500

    finished accept and complete beacon

--- beacon_backend/index.js
+++ beacon_backend/index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000
 
 // database integration
 const dburi = "mongodb+srv://sateendradey:WordPass1990!@cluster0-wgoht.mongodb.net/test?retryWrites=true";
+const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 var db;
 
@@ -20,12 +21,18 @@ app.get('/', (req, res) => res.send('Hello World!'))
 // beacon end points
 
 app.post('/createbeacon', function (req, res) {
+    var dateTime = getCurrDateTime();
    const new_beacon = {
+       title: req.body.title,
        desc: req.body.desc,
+       locdesc: req.body.locdesc,
        lat: req.body.lat,
        lon: req.body.lon,
-       time: req.body.time,
-       userid: req.body.userid,
+       time: dateTime,
+       uname: req.body.uname,
+       lnd: req.body.lnd,
+       do_uname:'X',
+       color:'none',
        status: '0' 
    };
     CreateBeacon(new_beacon, function(response){
@@ -35,7 +42,11 @@ app.post('/createbeacon', function (req, res) {
 });
 
 //complete beacon
-app.post('/completebeacon', function (req, res) {   
+app.post('/completebeacon', function (req, res) { 
+    CompleteBeacon(req.body, function(response){
+	   console.log(response);
+        res.send(response);
+    });
 });
 
 //user end points
@@ -53,6 +64,13 @@ app.get('/profile/:id', function (req, res) {
 // get active becons nearby
 app.get('/activebeacons', function (req, res) {
      GetBeacon(function(response){
+	   console.log(response);
+        res.send(response);
+    });
+});
+//get in progress beacons
+app.get('/progressbeacons', function (req, res) {
+     GetBeacon_inProgress(function(response){
 	   console.log(response);
         res.send(response);
     });
@@ -91,19 +109,19 @@ app.post('/login', function(req, res){
 });
 
 // Accept beacon
+app.post('/beaconaccept', function(req, res){
+    acceptBeacon(req.body,function(response){
+       console.log(response);
+        res.send(response);    
+    });
+});
 
-// Logout : change status
-
-// get beacon status
-
-
-
-
-
-
-
-
-
+app.post('/inc',function(req, res){
+    increaseCredit('amitr',function(response){
+       console.log(response);
+        res.send(response);    
+    });
+});
 
 
 function AddProfile(new_prof, callback){
@@ -129,8 +147,20 @@ function GetBeacon(callback){
     });
 }
 
+function GetBeacon_inProgress(callback){
+    var res;
+    var dbo = db.db("Beacon_DB");
+    var searchTerm = { status: "1" };
+   	console.log(searchTerm);
+    dbo.collection("Beacons").find(searchTerm).toArray(function(err, result) {
+        if (err) throw err;
+        res =  result;
+        return callback(res);
+    });
+}
+
+
 function CreateBeacon(new_beacon, callback){
-    console.log("in beacon post");
    var dbo = db.db("Beacon_DB");
   dbo.collection('Beacons').insertOne(new_beacon, function (err, result) {
       if (err)
@@ -153,8 +183,9 @@ function getProfile(user_id, callback) {
 }
 
 function processLogin(body_obj,callback){
+    // pass lat, lon, airl, gate
   var dbo = db.db("User_DB");
-  var myquery = { uname: body_obj.userid };
+  var myquery = { uname: body_obj.uname };
   var dateTime = getCurrDateTime();  
   var newvalues = { $set: {lasttime: dateTime, lat: body_obj.lat, lon:body_obj.lon, airl:body_obj.airl,  gate:body_obj.gate} };
   dbo.collection("Details_Cols").updateOne(myquery, newvalues, function(err, res) {
@@ -162,6 +193,54 @@ function processLogin(body_obj,callback){
     console.log("1 document updated");
       callback("Success");
   });
+}
+
+function acceptBeacon(body_obj,callback){
+    // pass bid, do_uname
+    var dbo = db.db("Beacon_DB");
+    var color = getRandomColor();
+    var o_id = new mongo.ObjectID(body_obj.bid );
+    var myquery = { _id: o_id};
+    var dateTime = getCurrDateTime();  
+    var newvalues = { $set: {do_uname: body_obj.do_uname, status:'1', color:color} };
+    dbo.collection("Beacons").updateOne(myquery, newvalues, function(err, res) {
+        if (err) {callback("fail")}
+        console.log("1 document updated");
+          callback("Success");
+      });
+}
+
+function CompleteBeacon(body_obj,callback){
+    // pass bid, do_uname
+    var dbo = db.db("Beacon_DB");
+    var o_id = new mongo.ObjectID(body_obj.bid );
+    var myquery = { _id: o_id};
+    var dateTime = getCurrDateTime();
+    increaseCredit(body_obj.do_uname);
+    var newvalues = { $set: {do_uname: body_obj.do_uname, status:'2',color:'none'} };
+    dbo.collection("Beacons").updateOne(myquery, newvalues, function(err, res) {
+        if (err) {callback("fail")}
+        console.log("1 document updated");
+          callback("Success");
+      });
+}
+
+function increaseCredit(user_id){
+    var dbo = db.db("User_DB");
+    var myquery =  { uname: user_id };
+    var dateTime = getCurrDateTime();  
+    var newvalues = {$inc:{"credits":1}};
+    dbo.collection("Details_Cols").updateOne(myquery, newvalues, function(err, res) {
+        if (err) {console.log("fail");return "fail";}
+        else{
+        console.log("1 document updated");
+          return "Success";
+        }
+      });
+}
+
+function getRandomColor(){
+    return 'green';
 }
 
 function distance(lat1, lon1, lat2, lon2, unit) {
